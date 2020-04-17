616bd8242bccc83b61d336ec680cf4c46099e4a4
rc <2432825911@qq.com>
2020-04-17 23:02:20 +0800

    a redis learn code

---
+++ springboot/redis/src/main/java/com/rc/test/redis/service/UmsMemberService.java
@@ -0,0 +1,16 @@
+package com.rc.test.redis.service;
+
+import com.rc.test.redis.commom.CommonResult;
+
+public interface UmsMemberService {
+
+    /**
+     * 生成验证码
+     */
+    CommonResult generateAuthCode(String telephone);
+
+    /**
+     * 判断验证码和手机号码是否匹配
+     */
+    CommonResult verifyAuthCode(String telephone, String authCode);
+}
