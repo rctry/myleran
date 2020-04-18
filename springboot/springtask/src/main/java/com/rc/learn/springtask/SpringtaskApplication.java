598407a095b2a784807f0d0e8f750410072c7030
rc <2432825911@qq.com>
2020-04-18 14:41:23 +0800

    spring task

---
+++ springboot/springtask/src/main/java/com/rc/learn/springtask/SpringtaskApplication.java
@@ -0,0 +1,14 @@
+package com.rc.learn.springtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+public class SpringtaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringtaskApplication.class, args);
+    }
+
+}
